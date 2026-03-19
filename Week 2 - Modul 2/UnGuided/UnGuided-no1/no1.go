6de4a4bf9d351aa6fc38321a2ebe7f89064d5151
IrfanAli2207 <ali.irfan2207@gmail.com>
2026-03-19 15:39:20 +0700

    init struktur baru

---
+++ Week 2 - Modul 2/UnGuided/UnGuided-no1/no1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tahun int
+
+	fmt.Print("Masukkan tahun: ")
+	fmt.Scan(&tahun)
+
+	if tahun%400 == 0 || (tahun%4 == 0 && tahun%100 != 0) {
+		fmt.Println("Tahun :", tahun)
+		fmt.Println("Kabisat : True")
+	} else {
+		fmt.Println("Tahun :", tahun)
+		fmt.Println("Kabisat : False")
+	}
+}
